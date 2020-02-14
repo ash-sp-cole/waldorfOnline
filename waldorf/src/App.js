849dadc8e57124368d2a69553a1e4c7b2df0bc61
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-02-14 10:55:30 +0100

    redux practice

--- waldorf/src/App.js
+++ waldorf/src/App.js
@@ -1,86 +1,25 @@
 import React, { Component } from 'react';
-import Display from "./Display/display";
-import Search from './search';
-import Validation from './validation/validation';
-import Char from './charComponent/char';
+import Display from './Display/display';
 
+import { createStore } from 'redux';
+
+const initialState = {
+tech: 'react'
+
+};
+const store = createStore(reducer, initialState);
 
 class App extends Component {
 
-state = {
-userInput: ''
 
 
-}
-
-
-
-inputChangeHandler = (e)=>{
-
-this.setState({
-  userInput: e.target.value
-})
-
-
-
-}
-
-deleteChangeHandler = (index) =>{
-
-
-const text = this.state.userInput.split('');
-text.splice(index,1);
-const updateText = text.join('');
-this.setState({
-  userInput: updateText
-})
-
-}
-
-
-
-
-
-
-render(){
-
-const charList = this.state.userInput.split('').map((ch, index) => {
-
-  return <Char  
-  character={ch} 
-  key={index}
-  clicked={()=> this.deleteChangeHandler(index)}
-  />
-
-
-
-});
-
-
-return (
-
-<div>
-
- <input type ='text' 
- onChange= {this.inputChangeHandler} 
-  value={this.state.userInput}></input> 
-  <p> 
-
-    INLINE INPUT : 
-    {this.state.userInput}
-    <hr/>
-  </p>
-  <Display name={this.state.userInput}/>
-
-  <Validation size = {this.state.userInput.length}/>
-   {charList}
-</div>
-
-
-)
-
-
-
+render() {
+  return(
+    <div>
+      <h3> APP REDUX </h3>
+      <Display name="test state"/>
+    </div>
+  )
 }
 
 }
